2507bd05cd2198a91fea8ce2a0bb84f98bd91991
lixiang <wb-lx885246@alibaba-inc.com>
2020-12-28 11:09:57 +0800

    ADD 不同的二叉搜索树II

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ LeetCode学习记录
 BinaryTreeInorderTraversal  二叉树的中序遍历
 
 UniqueBinarySearchTrees 不同的二叉搜索树
+
+UniqueBinarySearchTreesII 不同的二叉搜索树II
---
+++ src/tree/UniqueBinarySearchTreesII.java
@@ -0,0 +1,58 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 不同的二叉搜索树 II
+ * <p>
+ * 给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+ * <p>
+ * 输入：3
+ * 输出：
+ * [
+ * [1,null,3,2],
+ * [3,2,null,1],
+ * [3,1,null,null,2],
+ * [2,1,3],
+ * [1,null,2,null,3]
+ * ]
+ * 解释：
+ * 以上的输出对应以下 5 种不同结构的二叉搜索树：
+ * <p>
+ * 1         3     3      2      1
+ * \       /     /      / \      \
+ * 3     2     1      1   3      2
+ * /     /       \                 \
+ * 2     1         2                 3
+ *
+ * @Author lixiang
+ * @Date 2020/12/28
+ */
+public class UniqueBinarySearchTreesII {
+    public List<TreeNode> generateTrees(int n) {
+        return helper(1, n);
+    }
+
+    public List<TreeNode> helper(int start, int end) {
+        List<TreeNode> list = new ArrayList<>();
+        if (start > end) {
+            list.add(null);
+            return list;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftList = helper(start, i - 1);
+            List<TreeNode> rightList = helper(i + 1, end);
+
+            for (TreeNode left : leftList) {
+                for (TreeNode right : rightList) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    list.add(root);
+                }
+            }
+        }
+        return list;
+    }
+}
