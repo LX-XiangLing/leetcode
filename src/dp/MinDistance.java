a2a5239f91c012f2185c9901200bea54deb51e5d
lixiang <wb-lx885246@alibaba-inc.com>
2021-03-17 23:08:24 +0800

    UPDATE  编辑距离

--- src/dp/MinDistance.java
+++ src/dp/MinDistance.java
@@ -6,38 +6,41 @@ package dp;
  */
 public class MinDistance {
     public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-
-        // 有一个字符串为空串
-        if (n * m == 0) {
-            return n + m;
+        int len1 = word1.length();
+        int len2 = word2.length();
+        // 多开一行一列是为了保存边界条件，即字符长度为 0 的情况，这一点在字符串的动态规划问题中比较常见
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        // 初始化：当 word2 长度为 0 时，将 word1 的全部删除即可
+        for (int i = 1; i <= len1; i++) {
+            dp[i][0] = i;
         }
-
-        // DP 数组
-        int[][] DP = new int[n + 1][m + 1];
-
-        // 边界状态初始化
-        for (int i = 0; i < n + 1; i++) {
-            DP[i][0] = i;
+        // 当 word1 长度为 0 时，插入所有 word2 的字符即可
+        for (int j = 1; j <= len2; j++) {
+            dp[0][j] = j;
         }
-        for (int j = 0; j < m + 1; j++) {
-            DP[0][j] = j;
-        }
-
-        // 计算所有 DP 值
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < m + 1; j++) {
-                int left = DP[i - 1][j] + 1;
-                int down = DP[i][j - 1] + 1;
-                int left_down = DP[i - 1][j - 1];
-                if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
-                    left_down += 1;
+        // 由于 word1.charAt(i) 操作会去检查下标是否越界，因此在 Java 里，将字符串转换成字符数组是常见的操作
+        char[] word1Array = word1.toCharArray();
+        char[] word2Array = word2.toCharArray();
+        // 递推开始，注意：填写 dp 数组的时候，由于初始化多设置了一行一列，横纵坐标有个偏移
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                // 这是最佳情况
+                if (word1Array[i - 1] == word2Array[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                    continue;
                 }
-                DP[i][j] = Math.min(left, Math.min(down, left_down));
+                // 否则在以下三种情况中选出步骤最少的，这是「动态规划」的「最优子结构」
+                // 1、在下标 i 处插入一个字符
+                int insert = dp[i][j - 1] + 1;
+                // 2、替换一个字符
+                int replace = dp[i - 1][j - 1] + 1;
+                // 3、删除一个字符
+                int delete = dp[i - 1][j] + 1;
+                dp[i][j] = Math.min(Math.min(insert, replace), delete);
+
             }
         }
-        return DP[n][m];
+        return dp[len1][len2];
     }
 
 }
